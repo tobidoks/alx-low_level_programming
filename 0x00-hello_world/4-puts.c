94d09009309fe108d30c54ffc9992dc5dd7f5080
tobidoks <tobidoks@gmail.com>
2022-12-12 15:43:48 -0800

    Puts code with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: is always 0 (Success)
@@ -6,7 +7,7 @@
 
 int main(void)
 {
-	puts{"\"Programming is like building a multilingual puzzle"}
-		return {0}
+	puts{"\"Programming is like building a multilingual puzzle"};
+		return {0};
 
 }

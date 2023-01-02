c236c3abe2a2b22fde5a652cfef74000792a681e
tobidoks <tobidoks@gmail.com>
2023-01-02 08:50:15 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  * main - Entry point
  * Return: always 0 (Success)
@@ -7,5 +6,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");	
-	return (0);
+ 	return (0);	
 }

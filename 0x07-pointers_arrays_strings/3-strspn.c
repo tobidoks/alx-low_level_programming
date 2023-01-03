851c0d02042de14c33d92a14e4eabe74afca33c7
tobidoks <tobidoks@gmail.com>
2023-01-03 07:24:18 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ *
+ * Return: the number of accepted bytes.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j i), bool;
+
+	for (i = 0; *(s +i) != '\0'; i++

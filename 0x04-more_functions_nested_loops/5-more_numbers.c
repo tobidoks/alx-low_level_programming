b8c57563b8e6b038931feb6eed7c69ffccb84591
tobidoks <tobidoks@gmail.com>
2023-01-03 20:15:42 -0800

    gggg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,11 @@
  * more_numbers - prints numbers between 0 to 14
  * 10 times.
  *
- 	
  * Return: no return.
- */void more_numbers(void)
-{int i, ch;
-	
+ */
+void more_numbers(void)
+{	int i, ch;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (ch = 0; ch < 15; ch++)

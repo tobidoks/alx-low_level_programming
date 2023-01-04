459ebca3f08aa24441a9f68960c6292d5ee9b746
tobidoks <tobidoks@gmail.com>
2023-01-04 03:41:16 -0800

    lets get it

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H_
 
 int _putchar(char);
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);

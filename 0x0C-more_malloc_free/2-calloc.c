4b6977310ce3f72d2d2aa21a86dcd1628f795899
tobidoks <tobidoks@gmail.com>
2023-01-12 11:54:21 -0800

    more commits on more calloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,10 +17,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size)i;
+	p = malloc(nmemb * size);
 
-		if (p == NULL)
-			return (NULL);
+	if (p == NULL)
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
